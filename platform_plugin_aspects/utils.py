1cf0c76081d8918ea5867b8d9709d4175446c375
shaharpcampusil <shaharp.campus.gov.il>
2026-03-08 16:26:45 +0200

    datset should be configurable

--- platform_plugin_aspects/utils.py
+++ platform_plugin_aspects/utils.py
@@ -133,8 +133,10 @@ def generate_guest_token(user, course, dashboards, filters) -> str:
     data = {
         "user": _superset_user_data(user),
         "resources": resources,
-        "rls": [{"clause": filter, "dataset": 811} for filter in formatted_filters],
+        "rls": [{"clause": filter, "dataset": superset_config.get("dataset_id", 811)} for filter in formatted_filters],
     }
+    
+    logger.info(f"RLS rules being sent: {data['rls']}")
 
     try:
         import requests
